d63625d8cff0c788f250408612f043ca00d4e03d
Sergey Dedkov <dsv.mail@yandex.ru>
2023-01-28 00:34:11 +0600

    commit dist

---
+++ packages/slate-react/dist/custom-types.d.ts
@@ -0,0 +1,17 @@
+import { BasePoint, BaseRange, BaseText, Path } from 'slate';
+import { ReactEditor } from './plugin/react-editor';
+declare module 'slate' {
+    interface CustomTypes {
+        Editor: ReactEditor;
+        Text: BaseText & {
+            placeholder?: string;
+        };
+        Range: BaseRange & {
+            placeholder?: string;
+        };
+        Point: BasePoint & {
+            basePath?: Path;
+        };
+    }
+}
+//# sourceMappingURL=custom-types.d.ts.map
---
+++ packages/slate/dist/interfaces/decoration.d.ts
@@ -0,0 +1,6 @@
+import { ExtendedType, BaseRange, Path } from '..';
+export interface BaseDecoration extends BaseRange {
+    basePath?: Path;
+}
+export declare type Decoration = ExtendedType<'Decoration', BaseDecoration>;
+//# sourceMappingURL=decoration.d.ts.map
